8d7b041a1feb30c3467727286af7b33a0490b1ea
Marta <56959222+MARTA292@users.noreply.github.com>
2022-01-30 22:10:42 +0100

    Add files via upload

---
+++ keys_gateway.php
@@ -0,0 +1,27 @@
+<?php
+	$aws_access_key_id='ASIAYMG3UJGYWYNGB5G2';
+	$aws_secret_access_key='jXzBTwMpvK73JLsCN+zSqhw8C9u+1mVz4LGxfy58';
+	$aws_session_token='FwoGZXIvYXdzEPX//////////wEaDFDFHE3D0T+7atFcCyLAAWKdZ84SuDG8tnPPHMRodm+ZwXyHq/4y21OTUPRtfJYR1dwEtc2sD2SSd5tC8QIKi70dRNHVzZFKLhA1Hi4q0ukyt7x3WzXr2+nVK87bgFB5uxbI2XQ0p0h8seEvluQ+z7oSE7GIXoS6gSEEYgqVxsYw8o7PpgJCcyYOgOxdQcbOunLD10LAyanS6g6wsr/xnbVoXjgjL4gNRo1o7WGJsBi0hY++ZU4HEnrAb/1w2N9uPgwUtGi1Jy3Xqa0Zbaj+zSim2tuPBjItP9FmEsrsip6tyrRC6sjFpI1CDXFVazvyD48fjnzSARZTLzSBm+mDT6OkX5Og';
+        
+	$lambda_func='s3keys';
+        $payload='{"queryStringParameters": { "ak":"' . $aws_access_key_id . 
+                    '", "sk":"' .$aws_secret_access_key .
+                    '", "st":"' . $aws_session_token . '" }}';
+
+        $cmd=' AWS_ACCESS_KEY_ID='. $aws_access_key_id .
+
+             ' AWS_SECRET_ACCESS_KEY='. $aws_secret_access_key .
+
+             ' AWS_SESSION_TOKEN='. $aws_session_token . ' aws lambda invoke --region us-east-1 --function-name '. $lambda_func . ' --payload \''. $payload . '\' /tmp/resp.json 2>&1';
+
+        exec( $cmd,$result,$result2);
+
+        header('Access-Control-Allow-Origin: *');
+
+        $result=file_get_contents("/tmp/resp.json");
+
+        $json=json_decode($result,true);
+
+        echo json_encode($json['body']);
+//      echo $payload;
+?>
